acb0e2637e8b5e189c0085ee2e880d07bfe3e1d3
shanon3 <shanonathooya@gmail.com>
2022-08-07 01:50:34 +0700

    init backend project

---
+++ backend/src/src/main/java/com/saga/gojapanese/GoJapaneseApplication.java
@@ -0,0 +1,13 @@
+package com.saga.gojapanese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoJapaneseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoJapaneseApplication.class, args);
+	}
+
+}
---
+++ backend/src/src/test/java/com/saga/gojapanese/GoJapaneseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.gojapanese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoJapaneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
